3e26ce8bffcf4d1306c618019e78a984877567c6
Kevin Schams <schamsk1@students.westerntc.edu>
2024-02-19 10:47:09 -0600

    completed step 2

---
+++ app/src/main/java/com/example/taskapp/TaskDetails.java
@@ -0,0 +1,32 @@
+package com.example.taskapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.taskapp.dataaccess.TaskDataAccess;
+import com.example.taskapp.models.Task;
+
+public class TaskDetails extends AppCompatActivity {
+
+    public static final String TAG = "TaskDetails";
+    public static final String EXTRA_TASK_ID = "taskId";
+    TaskDataAccess da;
+    Task task;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_details);
+
+        Intent i = getIntent();
+        long id = i.getLongExtra(EXTRA_TASK_ID, 0);
+        if (id > 0){
+            task = da.getTaskById(id);
+            Log.d(TAG, "id of task to get: " + id);
+        }else {
+            Log.d(TAG, "No task to get, create a new one.");
+        }
+    }
+}
--- app/src/main/java/com/example/taskapp/TaskListActivity.java
+++ app/src/main/java/com/example/taskapp/TaskListActivity.java
@@ -1,14 +1,61 @@
 package com.example.taskapp;
 
+import androidx.annotation.LongDef;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.taskapp.dataaccess.TaskDataAccess;
+import com.example.taskapp.models.Task;
+
+import java.util.ArrayList;
 
 public class TaskListActivity extends AppCompatActivity {
 
+    public static final String TAG = "TaskListActivity";
+
+    private ListView lsTasks;
+    private Button btnAddTask;
+    private TaskDataAccess da;
+    private ArrayList<Task> allTasks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
+
+        lsTasks = findViewById(R.id.lsTasks);
+        btnAddTask = findViewById(R.id.btnAddTask);
+        da = new TaskDataAccess(this);
+        allTasks = da.getAllTasks();
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TaskListActivity.this, TaskDetails.class);
+            }
+        });
+
+        ArrayAdapter<Task> adapter = new ArrayAdapter<Task>(this, android.R.layout.simple_list_item_1, allTasks);
+        lsTasks.setAdapter(adapter);
+
+        lsTasks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Log.d(TAG, position + "");
+                Task selectedTask = allTasks.get(position);
+//                Log.d(TAG, selectedTask.toString());
+                Intent i = new Intent(TaskListActivity.this, TaskDetails.class);
+                i.putExtra(TaskDetails.EXTRA_TASK_ID, selectedTask.getId());
+                startActivity(i);
+            }
+        });
     }
+
+
 }
